71fa37f85a2f9a7336e246d33c20f95d57f8c120
KoksalBerkay <73394080+KoksalBerkay@users.noreply.github.com>
2023-07-13 00:13:47 +0300

    update

--- common/utils.js
+++ common/utils.js
@@ -2,6 +2,17 @@ const utils = {};
 
 utils.flaggedUsers = [1663882102141, 1663900040545, 1664485938220];
 
+utils.styles = {
+    car: 'gray',
+    fish: 'red',
+    house: 'yellow',
+    tree: 'green',
+    bicycle: 'cyan',
+    guitar: 'blue',
+    pencil: 'magenta',
+    clock: 'lightgray',
+}
+
 utils.formatPercent = (n) => {
     return (n * 100).toFixed(2) + "%";
 }
