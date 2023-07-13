91e7d89f83cc17fd8df1aef17b98cd6f4593f723
KoksalBerkay <73394080+KoksalBerkay@users.noreply.github.com>
2023-07-13 12:57:37 +0300

    update

--- common/utils.js
+++ common/utils.js
@@ -3,26 +3,26 @@ const utils = {};
 utils.flaggedUsers = [1663882102141, 1663900040545, 1664485938220];
 
 utils.styles = {
-    car: 'gray',
-    fish: 'red',
-    house: 'yellow',
-    tree: 'green',
-    bicycle: 'cyan',
-    guitar: 'blue',
-    pencil: 'magenta',
-    clock: 'lightgray',
-}
+    car: { color: "gray", text: '🚗' },
+    fish: { color: "red", text: '🐟' },
+    house: { color: "yellow", text: '🏠' },
+    tree: { color: "green", text: '🌳' },
+    bicycle: { color: "cyan", text: '🚲' },
+    guitar: { color: "blue", text: '🎸' },
+    pencil: { color: "magenta", text: '✏️' },
+    clock: { color: "lightgray", text: '🕜' },
+};
 
 utils.formatPercent = (n) => {
     return (n * 100).toFixed(2) + "%";
-}
+};
 
 utils.printProgress = (count, max) => {
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
     const percent = utils.formatPercent(count / max);
     process.stdout.write(count + "/" + max + " (" + percent + ")");
-}
+};
 
 utils.groupBy = (objArray, key) => {
     const groups = {};
@@ -34,9 +34,8 @@ utils.groupBy = (objArray, key) => {
         groups[value].push(obj);
     }
     return groups;
-}
+};
 
-
-if (typeof module !== 'undefined') {
+if (typeof module !== "undefined") {
     module.exports = utils;
 }
