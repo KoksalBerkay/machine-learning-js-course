a7e49623eb25edc217410e420d5db6e3eee5e4ab
KoksalBerkay <73394080+KoksalBerkay@users.noreply.github.com>
2023-07-15 16:26:04 +0300

    update

--- common/utils.js
+++ common/utils.js
@@ -36,6 +36,29 @@ utils.groupBy = (objArray, key) => {
     return groups;
 };
 
+utils.distance = (p1, p2) => {
+    return Math.sqrt(
+        (p1[0] - p2[0]) ** 2 +
+        (p1[1] - p2[1]) ** 2
+    );
+}
+
+utils.getNearest = (loc, points) => {
+    let minDist = Number.MAX_SAFE_INTEGER;
+    let nearestIndex = 0;
+
+    for (let i = 0; i < points.length; i++) {
+        const point = points[i];
+        const d = utils.distance(loc, point);
+
+        if (d < minDist) {
+            minDist = d;
+            nearestIndex = i;
+        }
+    }
+    return nearestIndex;
+}
+
 if (typeof module !== "undefined") {
     module.exports = utils;
 }
