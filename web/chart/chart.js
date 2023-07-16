b0d3268e67572f35a5fee465ac6baa433253aa65
KoksalBerkay <73394080+KoksalBerkay@users.noreply.github.com>
2023-07-16 16:39:14 +0300

    update

--- web/chart/chart.js
+++ web/chart/chart.js
@@ -37,14 +37,16 @@ class Chart {
       this.defaultDataBounds = this.#getDataBounds();
 
       this.dynamicPoint = null;
+      this.nearestSample = null;
 
       this.#draw();
 
       this.#addEventListeners();
    }
 
-   showDynamicPoint(point) {
-      this.dynamicPoint = point;
+   showDynamicPoint(point, label, nearestSample) {
+      this.dynamicPoint = { point, label };
+      this.nearestSample = nearestSample;
       this.#draw();
    }
 
@@ -248,13 +250,25 @@ class Chart {
       }
 
       if (this.dynamicPoint) {
+         const { point, label } = this.dynamicPoint;
          const pixelLoc = math.remapPoint(
             this.dataBounds,
             this.pixelBounds,
-            this.dynamicPoint
+            point
          );
          graphics.drawPoint(ctx, pixelLoc, "rgba(255,255,255,0.7)", 10000000);
-         graphics.drawPoint(ctx, pixelLoc, "black");
+         ctx.beginPath();
+         ctx.moveTo(...pixelLoc);
+         ctx.lineTo(...math.remapPoint(
+            this.dataBounds,
+            this.pixelBounds,
+            this.nearestSample.point
+         ));
+         ctx.stroke();
+         graphics.drawImage(ctx,
+            this.styles[label].image,
+            pixelLoc,
+         );
       }
 
       this.#drawAxes();
